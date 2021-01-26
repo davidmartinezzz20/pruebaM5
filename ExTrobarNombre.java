00162a18573375781454432c26a0222d6ffe9c37
davisittto <78036074+davisittto@users.noreply.github.com>
2021-01-26 17:15:54 +0100

    Add files via upload

---
+++ ExTrobarNombre.java
@@ -0,0 +1,37 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author alumneT
+ */
+public class ExTrobarNombre {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         Scanner lector = new Scanner (System.in);
+        
+        int nombre, solucio=9;
+        
+        System.out.println("Introdueix un nombre entre 1 i 20: ");
+        nombre = lector.nextInt();
+        
+        if (nombre>20 || nombre<0) {
+            System.out.println("El nombre introduït no està entre 1 i 20.");
+        }
+        else if (nombre==solucio) {
+            System.out.println("Enhorabona, has encertat el nombre secret!");
+        }
+        else{
+            System.out.println("Aquest nombre no es correcte.");
+        }
+    }
+}
